b6cad2463eddd4b5f835e91abd1c87bc791f86d6
moizkh357 <moizkh357@gmai.com>
2023-04-05 14:54:24 +0500

    BSU-7--api-for-adding-books

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
-// Add books Api here 
-
+//books Api here 
+app.use("/api/books", require("./routes/booksRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 app.use(errorHandler);
 
