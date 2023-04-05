8accd4c754a207f6f2f0e82ddb7e92c9a10028c3
moizkh357 <moizkh357@gmai.com>
2023-04-05 15:43:21 +0500

    BSU-8--api-for-updating-and-deleting-books

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -21,7 +21,45 @@ const createBooks = asyncHandler(async (req, res) => {
   res.status(201).json(books);
 });
 
+//Update books
+const updateBooks = asyncHandler(async (req, res) => {
+  const books = await Books.findById(req.params.id);
+  if (!books) {
+    res.status(404);
+    throw new Error("Book not found");
+  }
+
+  if (books.user_id.toString() !== req.user.id) {
+    res.status(403);
+    throw new Error("User don't have permission to update other user books");
+  }
+
+  const updatedbook = await Books.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+
+  res.status(200).json(updatedbook);
+});
+
+//Delete book
+const deleteBooks = asyncHandler(async (req, res) => {
+  const books = await Books.findById(req.params.id);
+  if (!books) {
+    res.status(404);
+    throw new Error("Book not found");
+  }
+  if (books.user_id.toString() !== req.user.id) {
+    res.status(403);
+    throw new Error("User don't have permission to update other user books");
+  }
+  await Books.deleteOne({ _id: req.params.id });
+  res.status(200).json(books);
+});
+
+
 
 module.exports = {
-  createBooks
+  createBooks,updateBooks,deleteBooks
 };
--- routes/booksRoutes.js
+++ routes/booksRoutes.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const {
-  createBooks
+  createBooks,
+  updateBooks,
+  deleteBooks
 } = require("../controllers/booksController");
 const validateToken = require("../middleware/validateTokenHandler");
 
 
 router.use(validateToken);
 router.route("/").post(createBooks);
+router.route("/:id").put(updateBooks).delete(deleteBooks);
 
 module.exports = router;
